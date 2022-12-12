77b897e93c6d435f45f36d47b691ee5d53c73d7b
ZZhengLi <u6218016@au.edu.com>
2022-12-12 22:42:49 +0700

    revise

--- pages/AddCar_Qr.jsx
+++ pages/AddCar_Qr.jsx
@@ -6,36 +6,51 @@ import DownArrow from "../img/downArrow.png";
 import Qr from "../img/qr.png";
 import { useRouter } from "next/navigation";
 
-
-
-
 export default function AddCar_Qr() {
   const router = useRouter();
   return (
-    <div class = "flex flex-col space-y-4">
-      <div class = "align-content: center">
-        <Image src={Step1} width={250} alt="step1" class = "mx-auto max-w-lg h-auto" />
+    <div className="flex flex-col space-y-4">
+      <div className="align-content: center">
+        <Image
+          src={Step1}
+          width={250}
+          alt="step1"
+          className="mx-auto max-w-lg h-auto"
+        />
       </div>
-      <div class = "flex flex-col space-y-4">
-        <p class="text-center font-prompt text-[14px]">
-          Customers can register via Line to receive notification of receiving the car.
+      <div className="flex flex-col space-y-4">
+        <p className="text-center font-prompt text-[14px]">
+          Customers can register via Line to receive notification of receiving
+          the car.
         </p>
       </div>
-      <div class = "flex flex-col space-y-4">
-        <p class="text-center font-prompt text-[18px]">Scan now</p>
+      <div className="flex flex-col space-y-4">
+        <p className="text-center font-prompt text-[18px]">Scan now</p>
       </div>
-      {/* <div class = "flex flex-col space-y-4">
+      {/* <div className = "flex flex-col space-y-4">
         <p>down arrow icon</p>
       </div> */}
-      <div class = "align-content: center">
-        <Image src={DownArrow} alt="Down arrow" class = "object-scale-down h-5 w-5 mx-auto max-w-lg h-auto" />
+      <div className="align-content: center">
+        <Image
+          src={DownArrow}
+          alt="Down arrow"
+          className="object-scale-down h-5 w-5 mx-auto max-w-lg h-auto"
+        />
       </div>
-      <div class = "flex flex-col space-y-4">
-        <Image src={Qr} width={200} alt="qr" class = "mx-auto max-w-lg h-auto" />
+      <div className="flex flex-col space-y-4">
+        <Image
+          src={Qr}
+          width={200}
+          alt="qr"
+          className="mx-auto max-w-lg h-auto"
+        />
       </div>
-      <div class = "flex items-center justify-center">
-        <button class="bg-[#789BF3] hover:bg-[#789BF3] text-white font-bold py-2 px-4 rounded items-center" onClick={() => router.push("/AddCar_UploadFront_Before")}>
-          Next 
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-[#789BF3] hover:bg-[#789BF3] text-white font-bold py-2 px-4 rounded items-center"
+          onClick={() => router.push("/AddCar_UploadFront_Before")}
+        >
+          Next
         </button>
       </div>
       <div>
