f0b4d8bc3923a7cc1a68b1d1a4eef73ac37142df
ZZhengLi <u6218016@au.edu.com>
2022-12-15 19:35:24 +0700

    reupload

---
+++ pages/Setting.jsx
@@ -0,0 +1,12 @@
+import BottomNav from "./BottomNav";
+
+const Setting = () => {
+  return (
+    <div>
+      Setting
+      <BottomNav name="Setting" />
+    </div>
+  );
+};
+
+export default Setting;
